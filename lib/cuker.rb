586e7ca2a3e9b46cf5be32c2c990f31d61b8d2fc
ufo2mstar <ufo2mstar@gmail.com>
2019-05-09 19:43:36 -0400

    INIT: gem shell inited

---
+++ lib/cuker.rb
@@ -0,0 +1,6 @@
+require "cuker/version"
+
+module Cuker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cuker_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cuker do
+  it "has a version number" do
+    expect(Cuker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
